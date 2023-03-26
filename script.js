24de51bac5ad6f958ba515b69b16c26bde30c8dc
badanaut <49814350+badanaut@users.noreply.github.com>
2023-03-26 22:29:40 +0300

    Add today weather display

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-fetch('https://api.weatherapi.com/v1/current.json?key=6ed58b3a105f45a8b2565252232603&q=paris', { mode: 'cors' })
-  .then((response) => response.json())
-  .then((response) => {
-    console.log(response);
-  });
+const searchField = document.getElementById('search-input');
+const searchButton = document.getElementById('search');
+const cityName = document.getElementById('city-name');
+const timeInfo = document.getElementById('time-info');
+const weatherIcon = document.getElementById('weather-icon');
+const temperature = document.getElementById('temperature');
+const generalCondition = document.getElementById('temperature');
+const feelsLike = document.getElementById('feels-like');
+const airHumidity = document.getElementById('air-humidity');
+const windSpeed = document.getElementById('wind-speed');
+
+function displayWeatherInfo(city) {
+  fetch(`https://api.weatherapi.com/v1/current.json?key=6ed58b3a105f45a8b2565252232603&q=${city}`, { mode: 'cors' })
+    .then((response) => response.json())
+    .then((response) => {
+      cityName.textContent = city.charAt(0).toUpperCase() + city.slice(1);
+      timeInfo.textContent = response.location.localtime;
+      weatherIcon.src = response.current.condition.icon;
+      temperature.textContent = `${response.current.temp_c} °C`;
+      generalCondition.textContent = response.current.condition.text;
+      feelsLike.textContent = `Fells like: ${response.current.feelslike_c}  °C`;
+      airHumidity.textContent = `Humidity: ${response.current.humidity} %`;
+      windSpeed.textContent = `Wind speed: ${response.current.wind_kph} km/h`;
+    });
+}
+
+searchButton.addEventListener('click', () => {
+  const inputString = searchField.value;
+  displayWeatherInfo(inputString);
+});
+window.onload = displayWeatherInfo('sibiu');
